7c2f5a1ceef37e3bfed404e4af4816fdf7b28de8
BenLiyanage <ben@perfectresolution.com>
2012-07-14 18:59:57 -0400

    character info box is functional

--- UIUnitTest.py
+++ UIUnitTest.py
@@ -3,7 +3,7 @@ from pygame.locals import *
 import sprites
 from sprites import AnimatedSprite, Actor
 import ui
-from ui import Menu
+from ui import Menu, CharacterInfo
 
 pygame.init()
 
@@ -20,7 +20,7 @@ def main():
 
 	# Display some text
 
-	font = pygame.font.Font(None, 36)
+	font = pygame.font.Font('petme/PetMe128.ttf', 12)
 	text = font.render("Hello There", 1, (250, 250, 250))
 	textpos = text.get_rect()
 	textpos.centerx = background.get_rect().centerx
@@ -32,8 +32,11 @@ def main():
 	
 	menuItems = ['Attack','Wait','Special One', 'Special Two']
 	myMenu = Menu("Action:", menuItems, font, 50, 50, 200, 200)
+	tilesize=32
+	PrincessImageSet = sprites.load_sliced_sprites(64,64,'images/princess.png')
+    	PrincessSprite = Actor((23-.5)*tilesize, (21-1)*tilesize,PrincessImageSet[1], PrincessImageSet[0], PrincessImageSet[2], PrincessImageSet[3], "Peach", 3, 2, 2, 6, 60)
 
-
+	princessInfo = CharacterInfo(PrincessSprite, font, 600)
 	# Event loop
 	while 1:
 		for event in pygame.event.get():
@@ -44,7 +47,8 @@ def main():
 			if action is not None:
 				print action
 		screen.blit(background, (0, 0))
-		screen.blit(myMenu._menuBackground, myMenu.rect)		
+		screen.blit(myMenu.surface, myMenu.rect)		
+		screen.blit(princessInfo.surface, princessInfo.rect)
 		pygame.display.flip()
 
 if __name__ == '__main__': main()
--- ui.py
+++ ui.py
@@ -3,35 +3,72 @@ from pygame.locals import *
 
 pygame.init()
 
+FONTCOLOR = (10, 10, 10)
+BACKGROUNDCOLOR = (250,250,250)
+
 class InfoBox(object):
-	def __init__(self):
-		self._title = "placeholder"
-
-class CharacterInfo(object):
-	def __init__(self, character):
-		self._background = pygame.Surface((100, 200))
-		self._background.convert()
-		self._background.fill((250,250,250))
-
-class Menu(object):
-
-	def __init__(self, title, menuItems, font, x, y, width, height):
+	def __init__(self, title, font, x, y, width, height):
 
 		self._font = font
-		self._title = self._font.render(title, 0, (10, 10, 10))
+
+		self._title = title
+		self._title = self._font.render(title, 0, FONTCOLOR)
 		self._titlePosition = self._title.get_rect()
 		self._titlePosition.top = 0
 		self._titlePosition.left = 0
+
+		self.surface = pygame.Surface((width, height))
+		self.surface.convert()
+		self.surface.fill(BACKGROUNDCOLOR)
+		self.surface.blit(self._title, self._titlePosition)
+
+		self.rect = self.surface.get_rect()
+		self.rect.top = y
+		self.rect.left = x
+
+class CharacterInfo(InfoBox):
+	def __init__(self, character, font, screenHeight):
+		super(CharacterInfo,self).__init__(character._Name, font, 0, screenHeight-100, 300, 150)
+		self._toolTips = []
+
+		# Print Stats in columns
+
+		# First Column
+		xOffset = 10
+		yOffset = self._titlePosition.bottom+10
+
+		yOffset = self.AddTip("Level: " + str(character.Level()), "", xOffset, yOffset) + 5
+		yOffset = self.AddTip("Power: " + str(character.Power()), "", xOffset, yOffset) + 5
+		self.AddTip("Speed: " + str(character.Speed()), "", xOffset, yOffset)
+
+		# Second Column
+		xOffset = 125
+		yOffset = self._titlePosition.bottom+10
+
+		yOffset = self.AddTip("Experience: " + str(character.Experience()), "", xOffset, yOffset) + 5
+		yOffset = self.AddTip("Defense: " + str(character.Defense()), "", xOffset, yOffset) + 5
+		self.AddTip("Movement: " + str(character.Movement()), "", xOffset, yOffset)
+
+	def AddTip(self, label, description, x, y):
+		tip = {}
+		tip['surface'] = self._font.render(label, 0, FONTCOLOR)
+		tip['rect'] = tip['surface'].get_rect()
+		tip['rect'].top = y
+		tip['rect'].left = x
+		tip['description'] = description
+		self._toolTips.append(tip)
+
+		self.surface.blit(tip['surface'],tip['rect'])
+
+		return tip['rect'].bottom
+
+class Menu(InfoBox):
+
+	def __init__(self, title, menuItems, font, x, y, width, height):
+
+		super(Menu,self).__init__(title, font, x, y, width, height)
+
 		self._currentMenuItem = 0
-
-		self._menuBackground = pygame.Surface((width, height))
-		self._menuBackground = self._menuBackground.convert()
-		self._menuBackground.fill((250,250,250))
-		self._menuBackground.blit(self._title, self._titlePosition)
-
-		self.rect = self._menuBackground.get_rect()
-		self.rect.top = x
-		self.rect.left = y
 
 		self._indent = 40
 		itemY = self._titlePosition.bottom + 10
@@ -41,20 +78,20 @@ class Menu(object):
 		for item in menuItems:
 			menuItem = {}
 			menuItem['name'] = item
-			menuItem['surface'] = self._font.render(item, 0, (10,10,10))			
+			menuItem['surface'] = self._font.render(item, 0, FONTCOLOR)
 			menuItem['position'] = menuItem['surface'].get_rect()
 			menuItem['position'].top = itemY
 			menuItem['position'].left = itemX
 			itemY = menuItem['position'].bottom + 5
-			self._menuBackground.blit(menuItem['surface'], menuItem['position'])		
+			self.surface.blit(menuItem['surface'], menuItem['position'])		
 			self._menuItems.append(menuItem)
 
 	def setActive(self, itemNumber):
-		self._menuItems[self._currentMenuItem]['surface'] = self._font.render(self._menuItems[self._currentMenuItem]['name'],0,(10,10,10), (250, 250, 250))
-		self._menuBackground.blit(self._menuItems[self._currentMenuItem]['surface'], self._menuItems[self._currentMenuItem]['position'])
+		self._menuItems[self._currentMenuItem]['surface'] = self._font.render(self._menuItems[self._currentMenuItem]['name'],0,FONTCOLOR, BACKGROUNDCOLOR)
+		self.surface.blit(self._menuItems[self._currentMenuItem]['surface'], self._menuItems[self._currentMenuItem]['position'])
 
-		self._menuItems[itemNumber]['surface'] = self._font.render(self._menuItems[itemNumber]['name'],0,(250,250,250), (10,10,10))
-		self._menuBackground.blit(self._menuItems[itemNumber]['surface'], self._menuItems[itemNumber]['position'])
+		self._menuItems[itemNumber]['surface'] = self._font.render(self._menuItems[itemNumber]['name'],0,BACKGROUNDCOLOR, FONTCOLOR)
+		self.surface.blit(self._menuItems[itemNumber]['surface'], self._menuItems[itemNumber]['position'])
 
 		self._currentMenuItem = itemNumber
 
